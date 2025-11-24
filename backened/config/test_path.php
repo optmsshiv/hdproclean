70a604dce276731039a7c54397688d9913492efa
optmsshiv <optmsshiv@gmail.com>
2025-11-24 08:56:15 +0530

    chck

---
+++ backened/config/test_path.php
@@ -0,0 +1,13 @@
+<?php
+echo "<h3>Testing PHPMailer Path</h3>";
+
+$path = __DIR__ . "/PHPMailer-master/src/PHPMailer.php";
+
+echo "Using path: $path<br>";
+
+if (file_exists($path)) {
+    echo "<span style='color:green'>FILE EXISTS ✔</span>";
+} else {
+    echo "<span style='color:red'>FILE NOT FOUND ❌</span>";
+}
+?>
