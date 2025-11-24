75d3329d055860ebb87bdbfbf49b5234e1a467dc
optmsshiv <optmsshiv@gmail.com>
2025-11-24 09:09:04 +0530

    `Refactored test_path.php: simplified path construction and file existence check`

--- backened/config/test_path.php
+++ backened/config/test_path.php
@@ -1,13 +1,6 @@
 <?php
-echo "<h3>Testing PHPMailer Path</h3>";
+$path = __DIR__ . '/../../PHPMailer/src/PHPMailer.php';
 
-$path = __DIR__ . "/../../PHPMailer/src/PHPMailer.php";
+echo $path . "<br>";
 
-echo "Using path: $path<br>";
-
-if (file_exists($path)) {
-    echo "<span style='color:green'>FILE EXISTS ✔</span>";
-} else {
-    echo "<span style='color:red'>FILE NOT FOUND ❌</span>";
-}
-?>
+echo file_exists($path) ? "✔ FOUND" : "❌ NOT FOUND";
